db6de844079d3376fe0ed59659ed330bcd3652f9
denis-dovgodko <dendov555@gmail.com>
2024-11-20 13:56:35 +0200

    Lab6.1 repository

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask 
+
+app = Flask (__name__)
+@app.route('/')
+@app.route('/home')
+
+def home():
+    return 'Hello world from app Pipeline testing.'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
